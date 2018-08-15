249f7ea4b0a1c61a5f88808f1e95fe8a1f6d3524
Brandon Lindsey <blindsey@seattlegoodwill.org>
2018-08-15 09:47:13 -0700

    Updated directories with foleders and classified different queries

---
+++ Zip_Code/Center_Diff_Zip_City.sql
@@ -0,0 +1,14 @@
+﻿--Which training center serves the students from the most different cities? 
+
+WITH city_count AS (
+SELECT program, zip_info.city, zip_info.county, zip_info.state, gwpd.py1718.zip, COUNT (zip_info.city) as cc, COUNT (gwpd.py1718.zip) as zc
+	FROM gwpd.py1718
+	LEFT JOIN gwpd.zip_info ON zip = zip_code
+	GROUP BY program, zip_info.city, zip_info.county, zip_info.state, gwpd.py1718.zip
+	ORDER BY COUNT (zip_info.city) desc
+	)
+
+SELECT program, COUNT(city_count.cc), COUNT(city_count.zc)
+FROM city_count
+GROUP BY program
+ORDER BY COUNT(city_count.zc) desc
