be689b449a293cf568f6d4d07954127fc0f99df5
Brandon Lindsey <blindsey@seattlegoodwill.org>
2018-08-17 21:38:39 -0700

    Added a Union query

---
+++ Gender_Age/Gender_Age_Program.sql
@@ -0,0 +1,14 @@
+﻿WITH ABE_Bellingham AS 
+(SELECT program, gender, age
+FROM gwpd.py1718
+WHERE program = 'ABE Bellingham'),
+
+ABE_Burien AS 
+(SELECT program, gender, age
+FROM gwpd.py1718
+WHERE program = 'ABE Burien')
+
+SELECT program, gender, age 
+FROM ABE_Bellingham
+UNION SELECT * FROM ABE_Burien
+ORDER BY age DESC
